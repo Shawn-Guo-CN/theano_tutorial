bee67977873b95b78995d6da464da9f6e3347467
Shawn <shawnguo.cn@gmail.com>
2016-07-29 11:16:40 +0800

    update the mnist data

--- logistic_regression_on_mnist.py
+++ logistic_regression_on_mnist.py
@@ -6,6 +6,11 @@ import cPickle
 import gzip
 
 def shared_dataset(data_xy):
+    """
+    Change the input data into theano shared variable
+    :param data_xy: tuple of input data
+    :return: tuple of shared data
+    """
     data_x, data_y = data_xy
     shared_x = theano.shared(np.asarray(data_x, dtype=theano.config.floatX))
     shared_y = theano.shared(np.asarray(data_y, dtype='int32'))
@@ -17,19 +22,16 @@ def load_data(file_name):
     :param file_name: the path and name of original data
     :return: list of three tuples, [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
     """
+    print "...loading data"
+
     with gzip.open(file_name, 'rb') as f:
-        train_set, valid_set, test_set = cPickle.load(f)
+        train_set, test_set = cPickle.load(f)
 
     test_set_x, test_set_y = shared_dataset(test_set)
-    valid_set_x, valid_set_y = shared_dataset(valid_set)
     train_set_x, train_set_y = shared_dataset(train_set)
-
-    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
+    rval = [(train_set_x, train_set_y), (test_set_x, test_set_y)]
     return rval
 
 if __name__ == '__main__':
     dataset = load_data('data/mnist.pkl.gz')
 
-    get_dataset01 = theano.function(inputs=[], outputs=dataset[0][1])
-
-    print dataset[0][0].get_value().shape, dataset[0][1].get_value().shape

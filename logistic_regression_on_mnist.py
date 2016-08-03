9d9210771af72093ec60b0b4110adab0a9ec87d5
Shawn <shawnguo.cn@gmail.com>
2016-08-03 10:09:57 +0800

    delete the useless function "shared_data()"

--- logistic_regression_on_mnist.py
+++ logistic_regression_on_mnist.py
@@ -4,18 +4,6 @@ import numpy as np
 
 import cPickle
 import gzip
-import time
-
-def shared_dataset(data_xy):
-    """
-    Change the input data into theano shared variable
-    :param data_xy: tuple of input data
-    :return: tuple of shared data
-    """
-    data_x, data_y = data_xy
-    shared_x = theano.shared(np.asarray(data_x, dtype=theano.config.floatX))
-    shared_y = theano.shared(np.asarray(data_y, dtype='int32'))
-    return shared_x, shared_y
 
 def load_data(file_name):
     """
